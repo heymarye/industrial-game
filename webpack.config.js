dcf963d2afc2983a8e2fd4da47a340e5ce5282d8
heymarye <82950840+heymarye@users.noreply.github.com>
2023-10-10 10:05:10 +0300

    public directory

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
   },
   resolve: {
     extensions: [".js"],
@@ -28,7 +28,7 @@ module.exports = {
     }),
   ],
   devServer: {
-    static: path.join(__dirname, "dist"),
+    static: path.join(__dirname, "public"),
     port: 3000,
   },
 };
